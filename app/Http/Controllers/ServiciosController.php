7ca596105187bb198535ac31a7c9c8cf6bc0c99a
Bely Mendoza <ruizullab@gmail.com>
2021-07-07 17:16:01 -0600

    show_examen

--- app/Http/Controllers/ServiciosController.php
+++ app/Http/Controllers/ServiciosController.php
@@ -8,58 +8,74 @@ use Yajra\DataTables\DataTables;
 
 class ServiciosController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $listarRamas = DB::Select('Select id_rama, descripcion from rama_examen');
 
         return view('servicios.index')->with('listarRamas', $listarRamas);
     }
 
-    public function listarRamas(){
+    public function listarRamas()
+    {
         $listarRamas = DB::Select('Select id_rama, descripcion from rama_examen');
 
         //return response()->json($listarRamas,200); 
         return view('servicios.formulario')->with('listarRamas', $listarRamas);
     }
 
-    public function guardarServicios(Request $request){
+    public function guardarServicios(Request $request)
+    {
         //Capturar Datos
-        $id_rama=$request->selectRama;
-        $nombre_servicio=$request->nombre_servicio;
-        $prefijo_servicio=$request->prefijo_servicio;
-        $precio_servicio=$request->precio_servicio;
-        $descripcion=$request->descripcion;
-        $borrado=$request->borrado;
+        $id_rama = $request->selectRama;
+        $nombre_servicio = $request->nombre_servicio;
+        $prefijo_servicio = $request->prefijo_servicio;
+        $precio_servicio = $request->precio_servicio;
+        $descripcion = $request->descripcion;
+        $borrado = $request->borrado;
 
         //Guardar Datos BD
-        $insert=DB::insert("INSERT INTO examen(id_rama, nombre_examen, prefijo, precio, descripcion, borrado, created_at) 
-                            VALUES (:id_rama, :nombre_examen, :prefijo, :precio, :descripcion, :borrado, now())", 
-                                    ["id_rama"=>$id_rama, "nombre_examen"=>$nombre_servicio, "prefijo"=>$prefijo_servicio, "precio"=>$precio_servicio, "descripcion"=>$nombre_servicio, "borrado"=>$borrado]);
+        $insert = DB::insert(
+            "INSERT INTO examen(id_rama, nombre_examen, prefijo, precio, descripcion, borrado, created_at) 
+                            VALUES (:id_rama, :nombre_examen, :prefijo, :precio, :descripcion, :borrado, now())",
+            ["id_rama" => $id_rama, "nombre_examen" => $nombre_servicio, "prefijo" => $prefijo_servicio, "precio" => $precio_servicio, "descripcion" => $nombre_servicio, "borrado" => $borrado]
+        );
 
         // return $request->all();
-        Session()->flash('examen','El examen ha sido guardado correctamente');
+        Session()->flash('examen', 'El examen ha sido guardado correctamente');
         return redirect()->route('servicios');
     }
 
-    public function getServicesData(){
-        $services=DB::SELECT('SELECT e.id_examen,re.descripcion as seccion, e.prefijo, e.nombre_examen, e.precio, e.created_at  FROM examen e
+    public function getServicesData()
+    {
+        $services = DB::SELECT('SELECT e.id_examen,re.descripcion as seccion, e.prefijo, e.nombre_examen, e.precio, e.created_at  FROM examen e
                                     LEFT JOIN rama_examen re ON e.id_rama=re.id_rama ');
 
         return DataTables::of($services)
-       
-        ->addColumn('action', 'servicios.action')
-        ->rawColumns(['action'])
-        ->make();
+
+            ->addColumn('action', 'servicios.action')
+            ->rawColumns(['action'])
+            ->make();
     }
 
-    public function show(Request $request){
-        $id_examen=$request->id_examen;
+    public function show(Request $request)
+    {
+        $id_examen = $request->id_examen;
 
-        $infoExamen=DB::Select('SELECT e.id_examen,re.descripcion as seccion, e.prefijo, e.nombre_examen, e.precio, e.created_at  FROM examen e
+        $infoExamen = DB::Select('SELECT e.id_examen,re.descripcion as seccion, e.prefijo, e.nombre_examen, e.precio, e.created_at  FROM examen e
                     LEFT JOIN rama_examen re ON e.id_rama=re.id_rama
-                    WHERE e.id_examen=:id_examen', ['id_examen'=>$id_examen]);
-        
+                    WHERE e.id_examen=:id_examen', ['id_examen' => $id_examen]);
+
         //return response()->json($infoExamen,200);
-        return view('servicios.show', ['nombre_examen'=>$infoExamen[0]->nombre_examen]); 
+        return view(
+            'servicios.show',
+            ['nombre_examen' => $infoExamen[0]->nombre_examen,
+            'id_examen' => $infoExamen[0]->id_examen,
+            'seccion' => $infoExamen[0]->seccion,
+            'prefijo' => $infoExamen[0]->prefijo,
+            'nombre_examen' => $infoExamen[0]->nombre_examen,
+            'precio' => $infoExamen[0]->precio,
+            'created_at' => $infoExamen[0]->created_at]
+        );
     }
 
     public function eliminarServicios(Request $request){
--- resources/views/servicios/show.blade.php
+++ resources/views/servicios/show.blade.php
@@ -3,12 +3,57 @@
 @section('title', 'Servicios')
 
 @section('content_header')
-    <h1>{{$nombre_examen}}</h1>
+<h1>{{$nombre_examen}}</h1>
 @stop
 
 @section('content')
-  
-    
+<div class="card text-center">
+    <div class="card-header">
+        Detalles del Servicio
+    </div>
+    <div class="card-body">
+        <div class="col-md-12">
+            <table class="table table-bordered table-striped">
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <td>{{$id_examen}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Sección</th>
+                        <td>{{$seccion}}</td>
+                    </tr>
+                    <tr>
+                        <th>Nombre del Examen</th>
+                        <td>{{$nombre_examen}}</td>
+                    </tr>
+                    <tr>
+                        <th>Prefijo</th>
+                        <td>{{$prefijo}}</td>
+                    </tr>
+                    <tr>
+                        <th>Precio</th>
+                        <td>{{$precio}}</td>
+                    </tr>
+                    <tr>
+                        <th>Creado</th>
+                        <td>{{$created_at}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            <div class="button-container">
+                <a href="{{ route('servicios') }}" class="btn btn-sm btn-success mr-3"> Volver </a>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
 @stop
 
 @section('css')
